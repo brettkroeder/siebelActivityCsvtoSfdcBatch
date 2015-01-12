1bb5d7f72a706df410a019c494274bdb0efbb76e
Brett Roeder <brett.roeder@mulesoft.com>
2015-01-11 19:21:29 -0500

    Problem starting Process Records phase

---
+++ src/test/resources/SFDC Activity Schema.groovy
@@ -0,0 +1,3 @@
+return [
+	['LX_Siebel_ID__c':'','What':'','Lx_Activity_Type__c':'','Subject':'','StartDateTime':'','EndDateTime':'','Status__c':'','Description':'','IsPrivate':'','CreatedDate':'','DurationInMinutes':'','SFDCObject':''] 
+];
